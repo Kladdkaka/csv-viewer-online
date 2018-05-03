2d36ac32904972687b8d2e7084fbacb9780b6803
Erik Wheeler <Kladdkaka@users.noreply.github.com>
2018-05-03 20:34:58 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,34 @@
-/* global FileReader, Papa, Handsontable */
+/* global Papa, Handsontable, readAsText */
+
+const getURLParameters = url =>
+  (url.match(/([^?=&]+)(=([^&]*))/g) || [])
+    .reduce(
+      (acc, value) => ({
+        ...acc,
+        [value.slice(0, value.indexOf('='))]: value.slice(value.indexOf('=') + 1)
+      }), {}
+    )
+
 const input = document.getElementById('input-file')
 const handsontableContainer = document.getElementById('handsontable-container')
 
-input.addEventListener('change', async function () {
+const params = getURLParameters(location.search)
+
+if (params.csv) {
+  createTable(btoa(unescape(params.csv)))
+} else {
+  input.addEventListener('change', async function () {
   const [file] = this.files
-  
+
   readAsText(file)
     .then(createTable)
     .catch(console.error)
-})
+  })
+}
 
 const createTable = csv => {
   input.remove()
-  
+
   const data = Papa.parse(csv, {
     header: true,
     skipEmptyLines: true
