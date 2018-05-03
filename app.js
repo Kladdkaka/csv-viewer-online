739531e42d8b51f06aedbe08509d4a10bee5cf73
Erik Wheeler <Kladdkaka@users.noreply.github.com>
2018-05-03 20:06:57 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 /* global FileReader, Papa, Handsontable */
-
 const input = document.getElementById('input-file')
 const handsontableContainer = document.getElementById('handsontable-container')
 
-input.onchange = function () {
-  var file = this.files[0]
-  var reader = new FileReader()
+input.addEventListener('change', function () {
+  const [file] = this.files
+  
+  const reader = new FileReader()
 
   reader.onload = function (e) {
     var csv = e.target.result
-    
+
     createTable(csv)
   }
 
   reader.readAsText(file)
-}
+})
 
 const createTable = csv => {
   var data = Papa.parse(csv, {
